5a288470969432a46d8be414ac8339c5c7819c24
Ittetsu Sato <ittetsu.sato@uzabase.com>
2023-01-01 16:35:30 +0900

    feat: 結果表示

--- funcs.js
+++ funcs.js
@@ -1,9 +1,12 @@
+const results = [""];
 const year = 2023;
+let gameFlg = false;
 const startButton = document.querySelector(".image");
 const rotate = document.querySelector(".rotate");
+const result = document.querySelector(".result");
+const messageArea = document.querySelector(".message");
 let resultNumber;
 
-const messageArea = document.querySelector(".message");
 var msg_buff = "";
 function message(msg) {
   if (msg_buff == "") {
@@ -23,22 +26,43 @@ function message_char() {
   if (c == "\n") {
     c = "<br>"; //改行の場合はタグへ変換
   }
+
   messageArea.innerHTML += c;
   //メッセージバッファから先頭1文字を削除
   msg_buff = msg_buff.slice(1);
   //
   setTimeout("message_char()", 60);
 }
+
 const rotateImage = setInterval(() => {
   const date = new Date();
   // 秒針:1周を60で割って、1秒あたりの角度を計算する
   const s = (360 / 60) * date.getSeconds();
-  rotate.style.transform = `rotate(${s}deg)`;
+  rotate.style.transform = `translate(-50%, -50%) rotate(${s}deg)`;
 }, 1000);
 
+const showResult = (resultNumber) => {
+  const title = `${resultNumber}等賞!`;
+  let resultMessage;
+  if (resultNumber === 0) {
+    resultMessage = "どんな困難も、一撃解決！";
+  }
+  message(title + resultMessage);
+  startButton.addEventListener("click", onClick);
+  startButton.style.pointerEvents = "auto";
+};
+
 const onClick = () => {
+  startButton.style.pointerEvents = "none";
+  messageArea.innerHTML = "";
+  if (gameFlg) {
+    message("返事がない、ただのフリーイラストのようだ。");
+    return;
+  }
+  gameFlg = true;
   startButton.classList.remove("rotate");
   clearInterval(rotateImage); //回転停止
+
   const today = new Date();
   const seconds = today.getSeconds();
   if (seconds === 0) {
@@ -47,7 +71,9 @@ const onClick = () => {
     resultNumber = year % seconds;
   }
   console.log({ resultNumber });
+
   message(`${year} を ${seconds} で割ったあまりは、、、      ${resultNumber}`);
+  setTimeout(() => showResult(resultNumber), 4000);
 };
 
 startButton.addEventListener("click", onClick);
